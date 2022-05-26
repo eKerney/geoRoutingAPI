f9208d3d6e473c5b60dd1e60aa2996f73b5c59f8
eKerney <Eric.Kerney@airspacelink.com>
2022-05-26 09:53:52 -0400

    deploying

--- geoRoutingAPI/settings.py
+++ geoRoutingAPI/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ey28xpa-k8q%7c2utqo=)u50f)_#^1-1(n4w8#&bp3e8ec12d_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
